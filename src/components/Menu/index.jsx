f0dace7c0c9d13f971cbfc05dd77d84709457173
ismaelrodino2 <ismael.roldino@hotmail.com>
2021-12-20 15:04:12 -0300

    Menu component + tests

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import P from 'prop-types';
+import * as Styled from './styles';
+import { SectionContainer } from '../SectionContainer';
+import { LogoLink } from '../LogoLink';
+import { NavLinks } from '../NavLinks';
+import { Menu as MenuIcon } from '@styled-icons/entypo/Menu';
+import { Close as CloseIcon } from '@styled-icons/evaicons-solid/Close';
+
+import { useState } from 'react';
+
+export const Menu = ({ links = [], logoData = {} }) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <Styled.Button
+        visible={visible}
+        onClick={() => setVisible(!visible)}
+        aria-label="Open/Close Menu"
+      >
+        {visible ? (
+          <CloseIcon aria-label="Close Menu" />
+        ) : (
+          <MenuIcon aria-label="Open Menu" />
+        )}
+      </Styled.Button>
+      <Styled.Container visible={visible} onClick={() => setVisible(false)}>
+        <SectionContainer>
+          <Styled.MenuContainer>
+            <LogoLink {...logoData} />
+            <NavLinks links={links} />
+          </Styled.MenuContainer>
+        </SectionContainer>
+      </Styled.Container>
+    </>
+  );
+};
+
+Menu.propTypes = {
+  ...NavLinks.propTypes,
+  logoData: P.shape(LogoLink.propTypes).isRequired,
+};
